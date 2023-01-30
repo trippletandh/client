006cc33f13e8ae9617b15f1405408e10bd24bbf0
TrongHien18 <67732499+chopper99it@users.noreply.github.com>
2023-01-30 17:32:22 +0700

    update

--- src/services/authService.js
+++ src/services/authService.js
@@ -14,7 +14,7 @@ const parseCookie = (str) =>
     .split(";")
     .map((v) => v.split("="))
     .reduce((acc, v) => {
-      acc[decodeURIComponent(v[0].trim())] = decodeURIComponent(v[1].trim());
+      acc[decodeURIComponent(v[0]?.trim())] = decodeURIComponent(v[1]?.trim());
       return acc;
     }, {});
 
